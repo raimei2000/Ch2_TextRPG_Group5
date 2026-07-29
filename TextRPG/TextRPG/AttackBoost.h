5556b1c4c0f10efdf9bee380cfab7ee9d8f9c79d
SonJiHyeop <ison0814@naver.com>
2026-07-29 14:34:20 +0900

    Merge branch 'main' into SonJiHyeop

---
+++ TextRPG/TextRPG/AttackBoost.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "Item.h"
+
+class Character;
+
+class AttackBoost : public Item {
+private:
+	std::string name_;
+	int attack_increase_;
+public:
+	AttackBoost();
+	std::string name() { return name_; };
+	void Use(Character* character);
+};
