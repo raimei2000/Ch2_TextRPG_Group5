87dd1a8911545c442ce75f098a64d31ecf671d86
keunhwan9852 <96756708+keunhwank@users.noreply.github.com>
2026-07-29 19:30:08 +0900

    Monster2

--- TextRPG/TextRPG/Monster.cpp
+++ TextRPG/TextRPG/Monster.cpp
@@ -1,30 +1,46 @@
-#include "Monster.h"
 #include <string>
+#include "Monster.h"
 
-Monster::Monster(std::string name, int health, int attack)
+
+Monster::Monster(std::string name_, int health_, int attack_)
 	:
-	name(name), health(health), attack(attack)
+	name_(name_), health_(health_), attack_(attack_)
 {
 
 }
 
 //getter
-std::string Monster::Name()
+std::string Monster::name()
 {
-	return name;
+	return name_;
 }
-int Monster::Health()
+int Monster::health()
 {
-	return health;
+	return health_;
 }
-int Monster::Attack()
+int Monster::attack()
 {
-	return attack;
+	return attack_;
 }
+
+//setter
+void Monster::set_name(std::string name_)
+{
+	this->name_ = name_;
+}
+void Monster::set_health(int health_)
+{
+	this->health_ = health_;
+}
+void Monster::set_attack(int attack_)
+{
+	this->attack_ = attack_;
+}
+
 
 
 //ÇÔ¼ö
-void takeDamage(int damage)
+void Monster::takeDamage(int damage)
 {
 	
 }
--- TextRPG/TextRPG/Monster.h
+++ TextRPG/TextRPG/Monster.h
@@ -6,18 +6,26 @@ class Monster
 {
 
 public:
-	std::string name;
-	int health; 
-	int attack;
-	Monster(std::string name, int health, int attack);
+	std::string name_;
+	int health_;
+	int attack_;
+	// 순수 가상 함수
+	Monster(std::string name_, int health_, int attack_);
 	
+    virtual void Attack() = 0;
 
 	//getter
-	std::string Name();
-	int Health();
-	int Attack();
+	std::string name();
+	int health();
+	int attack();
 
+	//setter
+	void set_name(std::string name_);
+	void set_health(int health_);
+	void set_attack(int attack_);
 
 	//함수
 	void takeDamage(int damage);
 };
+
+    
