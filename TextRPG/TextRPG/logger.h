089c399ea1829a2322dcbfd7b5bb71fc67003b5b
jwh2077 <jwh2077@gmail.com>
2026-07-29 18:37:55 +0900

    Log .cpp 작성

--- TextRPG/TextRPG/logger.h
+++ TextRPG/TextRPG/logger.h
@@ -3,26 +3,43 @@
 
 #include<iostream>
 #include<string>
+#include<map>
+
+struct MonsterLog
+{
+	int KillMonsterCounts = 0;
+	int DamageMonsterCounts = 0;
+};
+struct IteamLog
+{
+	int UseItemCounts = 0;
+	int GainItemCounts = 0;
+};
 
 class Logger
 {
+private:
+	std::map<std::string, MonsterLog>monsterLogs;
+	std::map<std::string, IteamLog>ItemLogs;
+	int totalGoldGained = 0;
+	int totalGoldUsed = 0;
+
 public:
 	Logger();
-	~Logger();
-	void RecordMonsterKill(std::string& monstername);
+	// ~Logger();
+	void RecordMonsterKill(const std::string& monstername);
 
-	void RecordMonsterDamage(std::string& monstername, int damage);
+	void RecordMonsterDamage(const std::string& monstername, int damage);
 
-	void RecordItemUse(std::string& itemname, int itemCount);
+	void RecordItemUse(const std::string& itemname, int itemCount);	
 
-	void RecordItemGain(std::string& itemname, int itemCount);
+	void RecordItemGain(const std::string& itemname, int itemCount);
 
-	void RecordGoldGain(int gold);
+	void RecordGoldGain(const int gold);
 
-	void RecordGoldUse(int gold);
+	void RecordGoldUse(const int gold);
 
-
-	//private:
+	void Log();
 
 };
 #endif	
