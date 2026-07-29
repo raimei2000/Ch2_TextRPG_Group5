43fa6c789e727f249f471b3029ebbfe2fd3e9539
jwh2077 <jwh2077@gmail.com>
2026-07-29 19:42:36 +0900

    Logger 주석 추가

--- TextRPG/TextRPG/logger.cpp
+++ TextRPG/TextRPG/logger.cpp
@@ -9,42 +9,42 @@
 
 void Logger::RecordMonsterKill(const std::string& monstername)
 {
-	monsterLogs[monstername].KillMonsterCounts++;
+	monsterLogs_[monstername].KillMonsterCounts++;
 }
 
 void Logger::RecordMonsterDamage(const std::string& monstername, int damage)
 {
-	monsterLogs[monstername].DamageMonsterCounts += damage;
+	monsterLogs_[monstername].DamageMonsterCounts += damage;
 }
 
 void Logger::RecordItemUse(const std::string& itemname, int itemCount)
 {
-	ItemLogs[itemname].UseItemCounts += itemCount;
+	ItemLogs_[itemname].UseItemCounts += itemCount;
 }
 
 void Logger::RecordItemGain(const std::string& itemname, int itemCount)
 {
-	ItemLogs[itemname].GainItemCounts += itemCount;
+	ItemLogs_[itemname].GainItemCounts += itemCount;
 }
 
 void Logger::RecordGoldGain(const int gold)
 {
-	totalGoldGained += gold;
+	totalGoldGained_ += gold;
 }
 
 void Logger::RecordGoldUse(const int gold)
 {
-	totalGoldUsed += gold;
+	totalGoldUsed_ += gold;
 }
 
 void Logger::Log() 
 {
-	for (auto monsterLog : monsterLogs)
+	for (auto monsterLog : monsterLogs_)
 	{
 		std::cout << "잡은 " << monsterLog.first << "의 마리 수 : " << monsterLog.second.KillMonsterCounts;
 		std::cout << "누적 데미지 : " << monsterLog.second.DamageMonsterCounts << std::endl;
 	}	
-	for (auto itemLog : ItemLogs)
+	for (auto itemLog : ItemLogs_)
 	{
 		std::cout << "얻은" << itemLog.first << "의 개수 : " << itemLog.second.GainItemCounts;
 		std::cout << "사용한 아이템의 개수 : " << itemLog.second.UseItemCounts << std::endl;
