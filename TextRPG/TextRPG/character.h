45947f7c8b48c65d12f88d1adfa7b35a7707c0e9
SonJiHyeop <ison0814@naver.com>
2026-07-29 21:23:16 +0900

    레벨업 로직을 level_up.cpp로 분리

---
+++ TextRPG/TextRPG/character.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <string>
+
+class Character {
+protected:
+std::string name_;
+
+int level_;
+int max_level_;
+
+int hp_;
+int max_hp_;
+int power_;
+int defence_;
+
+int exp_;
+int max_exp_;
+
+void LevelUp();
+
+public:
+explicit Character(const std::string& name);
+
+const std::string& name() const;
+int level() const;
+int hp() const;
+int max_hp() const;
+int power() const;
+int defence() const;
+int exp() const;
+int max_exp() const;
+
+void set_hp(int new_hp);
+void set_name(const std::string& new_name);
+
+void GainExp(int amount);
+};
