c3e4dc7320f82a86462b03d39c7492528e4af2b6
keunhwan9852 <96756708+keunhwank@users.noreply.github.com>
2026-07-29 20:16:54 +0900

    Monster->monster

--- TextRPG/TextRPG/Monster.h
+++ TextRPG/TextRPG/Monster.h
@@ -5,19 +5,30 @@
 class Monster
 {
 
+private:
+	std::string name_;
+	int health_;
+	int attack_;
+
+
 public:
-	std::string name;
-	int health; 
-	int attack;
+	// 순수 가상 함수
 	Monster(std::string name, int health, int attack);
-	
+
+	virtual void Attack() = 0;
 
 	//getter
-	std::string Name();
-	int Health();
-	int Attack();
+	std::string name();
+	int health();
+	int attack();
 
+	//setter
+	void set_name(std::string name);
+	void set_health(int health);
+	void set_attack(int attack);
 
 	//함수
-	void takeDamage(int damage);
+	void TakeDamage(int damage);
 };
+
+
