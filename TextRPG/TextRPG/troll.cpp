07a2a18939e72086c40170bd650c189b8e675c99
keunhwan9852 <96756708+keunhwank@users.noreply.github.com>
2026-07-30 15:06:00 +0900

    feat/트롤추가

---
+++ TextRPG/TextRPG/troll.cpp
@@ -0,0 +1,22 @@
+#include "troll.h"
+
+#include <iostream>
+
+#include "random_number_generator.h"
+
+
+
+// 플레이어 레벨에 비례한 랜덤 스탯으로 트롤 생성
+Troll::Troll(int player_level)
+  : Monster(
+    "Troll",
+    RandomNumberGenerator::RandomInteger(
+      player_level * 50, player_level * 70),
+    RandomNumberGenerator::RandomInteger(
+      player_level * 20, player_level * 30)) {
+}
+
+// 공격 문구
+void Troll::Attack(Character* character) {
+  std::cout << "Troll Attack!\n";
+}
