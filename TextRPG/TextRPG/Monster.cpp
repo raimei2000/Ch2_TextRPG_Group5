e1188a6133824fa434a917cea21bba5d2c086766
keunhwan9852 <96756708+keunhwank@users.noreply.github.com>
2026-07-30 12:51:46 +0900

    feat:/ add IsDead and TakeDamage functions

--- TextRPG/TextRPG/Monster.cpp
+++ TextRPG/TextRPG/Monster.cpp
@@ -1,47 +1,54 @@
 #include "monster.h"
 
-#include <string>
+#include <iostream>
 
+#include "character.h"
 
 Monster::Monster(std::string name, int health, int attack)
-	:
-	name_(name), health_(health), attack_(attack)
-{
-
+	: name_(name), health_(health), attack_(attack) {
 }
 
 //getter
-std::string Monster::name()
-{
+std::string Monster::name() {
 	return name_;
 }
-int Monster::health()
-{
+int Monster::health() {
 	return health_;
 }
-int Monster::attack()
-{
+int Monster::attack() {
 	return attack_;
 }
 
 //setter
-void Monster::set_name(std::string name)
-{
+void Monster::set_name(std::string name) {
 	this->name_ = name;
 }
-void Monster::set_health(int health)
-{
+void Monster::set_health(int health) {
 	this->health_ = health;
 }
-void Monster::set_attack(int attack)
-{
+void Monster::set_attack(int attack) {
 	this->attack_ = attack;
 }
 
 
 
-//함수
-void Monster::TakeDamage(int damage)
-{
+//몬스터의 체력 감소
+void Monster::TakeDamage(int damage) {
+	health_ - damage;
 
+	if ( health_ < 0 ) {
+		health_ = 0;
+	}
+}
+//몬스터 사망 여부 확인
+bool Monster::IsDead( ) const {
+	return health_ <= 0;;
+}
+//몬스터 스텟창 확인
+void Monster::PrintMonsterStatus( ) const {
+	std::cout << "===== Monster Status =====" << '\n';
+	std::cout << "Name   : " << name_ << '\n';
+	std::cout << "Health : " << health_ << '\n';
+	std::cout << "Attack : " << attack_ << '\n';
+	std::cout << "============================" << '\n';
 }
--- TextRPG/TextRPG/goblin.h
+++ TextRPG/TextRPG/goblin.h
@@ -1,11 +1,11 @@
 #pragma once
+
 #include "monster.h"
 
-class Goblin : public Monster
-{
+class Goblin : public Monster {
 public:
 
 	Goblin(int player_level);
 
-	void Attack() override;
+	void Attack(Character* character) override;
 };
