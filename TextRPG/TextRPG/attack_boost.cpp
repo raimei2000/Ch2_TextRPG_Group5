afc89e89ff30f2b39a7ece3f5183eb1b69012b9e
sirinakuse <bup2689@gmail.com>
2026-07-31 15:26:46 +0900

    refactor/item
    hp포션, 공격력 부스트 멘트추가

--- TextRPG/TextRPG/attack_boost.cpp
+++ TextRPG/TextRPG/attack_boost.cpp
@@ -1,4 +1,7 @@
 #include "attack_boost.h"
+
+#include <iostream>
+
 #include "character.h"
 
 AttackBoost::AttackBoost() {
@@ -11,5 +14,8 @@ std::string AttackBoost::name() {
 }
 
 void AttackBoost::Use(Character* character) {
+	int prev_power_ = character->power( );
 	character->AddTemporaryPowerBonus(attack_increase_);
+	std::cout << character->name( ) << "가 " << name_ << "를 사용했습니다." << std::endl;
+	std::cout << "공격력: " << prev_power_ << " -> " << character->power( );
 }
