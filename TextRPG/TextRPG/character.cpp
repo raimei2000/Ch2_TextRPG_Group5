9b3e8043b1643497449f33976aaf856ea015a3bc
SonJiHyeop <ison0814@naver.com>
2026-07-31 11:05:58 +0900

    feat/UseItem()logger & cout

--- TextRPG/TextRPG/character.cpp
+++ TextRPG/TextRPG/character.cpp
@@ -62,55 +62,55 @@ return gold_;
 }
 
 void Character::GainGold(int amount) {
-if ( amount <= 0 ) {
-return;
-}
-//현재 보유골드 갱신
-gold_ += amount;
+  if ( amount <= 0 ) {
+    return;
+  }
+  //현재 보유골드 갱신
+  gold_ += amount;
 
-//총 획득골드 기록
-if ( logger_ != nullptr ) {
-logger_->RecordGoldGain(amount);
-}
+  //총 획득골드 기록
+  if ( logger_ != nullptr ) {
+    logger_->RecordGoldGain(amount);
+  }
 }
 
 
 bool Character::UseGold(int amount) {
-if ( amount <= 0 || amount > gold_ ) {
-  return false;
-}
-gold_ -= amount;
+  if ( amount <= 0 || amount > gold_ ) {
+    return false;
+  }
+  gold_ -= amount;
 
-//총 사용골드 기록
-if ( logger_ != nullptr ) {
-  logger_->RecordGoldUse(amount);
-}
+  //총 사용골드 기록
+  if ( logger_ != nullptr ) {
+    logger_->RecordGoldUse(amount);
+  }
   return true;
 }
 
 void Character::set_hp(int new_hp) {
-if (new_hp < 0) {
-hp_ = 0;
-}
-else if (new_hp > max_hp_) {
-hp_ = max_hp_;
-}
-else {
-hp_ = new_hp;
-}
+  if ( new_hp < 0 ) {
+    hp_ = 0;
+  }
+  else if ( new_hp > max_hp_ ) {
+    hp_ = max_hp_;
+  }
+  else {
+    hp_ = new_hp;
+  }
 }
 
 void Character::set_power(int new_power) {
-if (new_power < 0){
-power_ = 0;
-}
-else {
-power_ = new_power;
-}
+  if ( new_power < 0 ) {
+    power_ = 0;
+  }
+  else {
+    power_ = new_power;
+  }
 }
 
 void Character::set_name(const std::string& new_name) {
-name_ = new_name;
+  name_ = new_name;
 }
 
 
@@ -149,47 +149,60 @@ monster->TakeDamage(power_);
 
 //아이템 추가
 void Character::AddItem(std::unique_ptr<Item> item) {
-if ( item == nullptr ) { //전달받은 아이템 없으면 종료
-return;
-}
-//uniqe_ptr 이동 전에 아이템 이름 저장
-const std::string item_name = item->name( );
+  if ( item == nullptr ) { //전달받은 아이템 없으면 종료
+    return;
+  }
+  //uniqe_ptr 이동 전에 아이템 이름 저장
+  const std::string item_name = item->name( );
 
-//logger 연결돼있으면 아이템 획득 기록
-inventory_.push_back(std::move(item));
-if ( logger_ != nullptr ) {
-logger_->RecordItemGain(item_name);
-}
+  //logger 연결돼있으면 아이템 획득 기록
+  inventory_.push_back(std::move(item));
+  if ( logger_ != nullptr ) {
+    logger_->RecordItemGain(item_name);
+  }
 
-std::cout << item_name << " 아이템을 획득했습니다." << std::endl;
+  std::cout << item_name << " 아이템을 획득했습니다." << std::endl;
 }
 
 //인벤토리 크기 확인
 int Character::inventory_size( ) const {
-return static_cast<int> (inventory_.size( ));
+  return static_cast<int> (inventory_.size( ));
 }
 
 //아이템 사용
 void Character::UseItem(int index) {
-if ( index < 0 || index >= inventory_size( ) ) {
-return;
-}
-inventory_[ index ]->Use(this);
+  if ( index < 0 || index >= inventory_size( ) ) {
+    return;
+  }
 
-//사용한 아이템 제거
-inventory_.erase(inventory_.begin( ) + index);
+  //아이템이 삭제되기 전에 이름 저장
+  const std::string item_name = inventory_[ index ]->name( );
+
+  //아이템 효과 적용
+  inventory_[ index ]->Use(this);
+
+  //사용한 아이템 출력
+  std::cout << item_name << " 을 사용했습니다." << std::endl;
+
+  //Logger에 아이템 사용기록 연동
+  if ( logger_ != nullptr ) {
+    logger_->RecordItemUse(item_name);
+  }
+
+  //사용한 아이템 제거
+  inventory_.erase(inventory_.begin( ) + index);
 }
 
 //인벤토리 표시
 void Character::DisplayInventory( ) const {
-std::cout << "========인벤토리========" << std::endl;
-if ( inventory_.empty( ) ) {
-std::cout << "인벤토리가 비어있습니다." << std::endl;
-}
-else {
-for ( int i = 0; i < inventory_size( ); i++ ) {
-std::cout << i << ". " << inventory_[ i ]->name( ) << std::endl;
-}
-}
-std::cout << "========================" << std::endl;
+  std::cout << "========인벤토리========" << std::endl;
+  if ( inventory_.empty( ) ) {
+    std::cout << "인벤토리가 비어있습니다." << std::endl;
+  }
+  else {
+    for ( int i = 0; i < inventory_size( ); i++ ) {
+      std::cout << i + 1 << ". " << inventory_[ i ]->name( ) << std::endl;
+    }
+  }
+  std::cout << "========================" << std::endl;
 }
