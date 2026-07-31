b99236ace9c3b285affd538ea89ef885b73b3b11
sirinakuse <bup2689@gmail.com>
2026-07-31 14:36:37 +0900

    refactor/item

--- TextRPG/TextRPG/health_potion.cpp
+++ TextRPG/TextRPG/health_potion.cpp
@@ -1,15 +1,22 @@
 #include "health_potion.h"
+
+#include <iostream>
+#include <algorithm>
+
 #include "character.h"
 
-HealthPotion::HealthPotion() {
+HealthPotion::HealthPotion( ) {
 	name_ = "HP포션";
 	health_restore_ = 50;
 }
 
-std::string HealthPotion::name() {
-	return name_; 
+std::string HealthPotion::name( ) {
+	return name_;
 }
 
 void HealthPotion::Use(Character* character) {
-	character->set_hp(character->hp() + health_restore_);
+	int prev_hp_ = character->hp( );
+	int healed_hp_ = std::min(character->hp( ) + health_restore_, character->max_hp( ));
+	std::cout << name_ << "를 사용했습니다." << std::endl << "HP: " << prev_hp_ << "->" << healed_hp_ << std::endl;
+	character->set_hp(healed_hp_);
 }
