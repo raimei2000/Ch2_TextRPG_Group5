b88e6f9eb436b523ee8a5f68d170d2215e277999
raimei <gigae0108@gmail.com>
2026-08-03 10:33:48 +0900

    refactor: 아이템 클래스들 수정

--- TextRPG/TextRPG/attack_boost.cpp
+++ TextRPG/TextRPG/attack_boost.cpp
@@ -4,16 +4,12 @@
 
 #include "character.h"
 
-AttackBoost::AttackBoost() {
-	name_ = "공격력 부스트";
-	attack_increase_ = 10;
-	price_ = 40;
-}
+AttackBoost::AttackBoost( ) : Item("공격력 부스트", 40), attack_increase_(10) {}
 
 void AttackBoost::Use(Character* character) {
 	int prev_power = character->power( );
 	character->AddTemporaryPowerBonus(attack_increase_);
 	std::cout << name_ << "를 사용했습니다." << std::endl;
-	std::cout << "공격력: " << prev_power << " -> " << character->power( )<< std::endl;
+	std::cout << "공격력: " << prev_power << " -> " << character->power( ) << std::endl;
 	std::cout << "============================" << std::endl;
 }
---
+++ TextRPG/TextRPG/item.cpp
@@ -0,0 +1,5 @@
+#include "item.h"
+
+#include <string>
+
+Item::Item(const std::string& name, int price) : name_(name), price_(price) {}
